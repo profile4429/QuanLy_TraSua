082a84cfcd5df322a7f55db684614622fd2626d5
profile4429 <19522141@gm.uit.edu.vn>
2021-06-04 11:11:48 +0700

    cơ sở dữ liệu

---
+++ QuanLiQuanTraSua/DTO/Bill.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiTraSua.DTO
+{
+    public class Bill
+    {
+        public Bill(DateTime? ngayvao, DateTime? ngayra, int id, int trangthai, int discount = 0)
+        {
+
+            this.Ngayvao = ngayvao;
+            this.Ngayra = ngayra;
+            this.ID = id;
+            this.Trangthai = trangthai;
+            this.Discount = discount;
+        }
+        public Bill(DataRow row)
+        {
+
+
+            this.Ngayvao = (DateTime?)row["ngayvao"];
+
+            var ngayraTemp = row["ngayra"];
+            if (ngayraTemp.ToString() != "")
+            this.Ngayra = (DateTime?)ngayraTemp;
+            this.ID = (int)row["id"];
+
+            this.Trangthai = (int)row["trangthai"];
+            if (row["discount"].ToString() != "")
+                this.Discount = (int)row["discount"];
+
+        }
+        private int discount;
+
+
+        private int trangthai;
+        private DateTime? ngayvao;
+        private DateTime? ngayra;
+        private int iD;
+
+        public int ID { get => iD; set => iD = value; }
+        public DateTime? Ngayra { get => ngayra; set => ngayra = value; }
+        public DateTime? Ngayvao { get => ngayvao; set => ngayvao = value; }
+        public int Trangthai { get => trangthai; set => trangthai = value; }
+        public int Discount { get => discount; set => discount = value; }
+    }
+}
---
+++ QuanLiQuanTraSua/DTO/BillInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiTraSua.DTO
+{
+    public class BillInfo
+    {
+        public BillInfo(int billID, int foodID, int id, int count)
+        {
+
+            this.BillID = billID;
+            this.FoodID = foodID;
+            this.ID = id;
+            this.Count = count;
+        }
+        public BillInfo(DataRow Row)
+        {
+
+            this.BillID = (int)Row["idbill"];
+            this.FoodID = (int)Row["idfood"];
+            this.ID = (int)Row["id"];
+            this.Count = (int)Row["soluong"];
+
+        }
+
+        private int count;
+        private int foodID;
+        private int billID;
+        private int iD;
+        public int ID { get => iD; set => iD = value; }
+        public int BillID { get => billID; set => billID = value; }
+        public int FoodID { get => foodID; set => foodID = value; }
+        public int Count { get => count; set => count = value; }
+    }
+}
