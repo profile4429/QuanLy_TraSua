48d5fc33ee10f156731419fc3f2034bb8bcf80af
Trần Anh Tuấn <19522481@gm.uit.edu.vn>
2021-06-16 23:08:15 +0700

    ui hoadon

---
+++ QuanLiQuanTraSua/Forms/FormHoaDon.cs
@@ -0,0 +1,26 @@
+﻿using QuanLiTraSua.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+namespace QuanLiQuanTraSua.Forms
+{
+    public partial class FormHoaDon : Form
+    {
+        public FormHoaDon()
+        {
+            InitializeComponent();
+        }  
+    }
+}
